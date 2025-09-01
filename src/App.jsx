3c04feff74ab2e98c30d2c12771601b88bfa10d6
vamus092 <dsuvam954@gmail.com>
2025-09-01 11:45:24 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Route , Routes } from 'react-router-dom'
+import {Link,Route , Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agents from './pages/Agents'
+import Projects from './pages/Projects'
 
 const App = () => {
   return (
-    <div className='text-#fff'>
+    <div className='text-white text-8xl'>
+
+      
+      <Link to='/'> Home</Link>
+      <Link to='/Agents'> Agents</Link>
+      <Link to='/Projects'> Projects</Link>
+
       <Routes>
-        <Route path=' / '            element={<Home />} />
-        <Route path=' /Agents ' element={<Agents  />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/Agents' element={<Agents />} />
+        <Route path='/Projects' element={<Projects />} />
       </Routes>
     </div>
   )
